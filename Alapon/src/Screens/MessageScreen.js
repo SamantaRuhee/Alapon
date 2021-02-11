e0ceee3d5223bf9288e5b642b04b925a5e9d9a2b
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2021-02-11 16:59:41 +0600

    Messages successfully stored in firebase.

--- Alapon/src/Screens/MessageScreen.js
+++ Alapon/src/Screens/MessageScreen.js
@@ -1,12 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { GiftedChat } from 'react-native-gifted-chat';
+import { AuthContext } from "../Provider/AuthProvider";
+import { useNetInfo } from "@react-native-community/netinfo";
+import * as firebase from "firebase";
+import "firebase/firestore";
 
-export default function MessageScreen() {
+const MessageScreen=()=> {
+  const netinfo = useNetInfo();
+  if (netinfo.type != "unknown" && !netinfo.isInternetReachable) {
+    alert("No Internet!");
+  }
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [messages, setMessages] = useState([
-    /**
-     * Mock message data
-     */
-    // example of system message
     {
       _id: 0,
       text: 'New room created.',
@@ -25,16 +31,66 @@ export default function MessageScreen() {
     }
   ]);
 
+  const loadPosts = async () => {
+    setLoading(true);
+    firebase
+      .firestore()
+      .collection("messages")
+      .orderBy("created_at", "desc")
+      .onSnapshot((querySnapshot) => {
+        let User = [];
+        querySnapshot.forEach((doc) => {
+          User.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        setPosts(User);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert(error);
+      });
+  };
+
+  useEffect(() => {
+    loadPosts();
+  }, []);
+
   // helper method that is sends a message
   function handleSend(newMessage = []) {
     setMessages(GiftedChat.append(messages, newMessage));
   }
 
   return (
+    <AuthContext.Consumer>
+      {(auth) => (
     <GiftedChat
       messages={messages}
-      onSend={newMessage => handleSend(newMessage)}
+      onSend={newMessage =>{
+        handleSend(newMessage);
+        setLoading(true);
+        firebase
+          .firestore().collection("messages").add({
+            userId: auth.CurrentUser.uid,
+            body: messages,
+            author: auth.CurrentUser.displayName,
+            created_at: firebase.firestore.Timestamp.now(),
+          })
+          .then(() => {
+            setLoading(false);
+            alert("Messages created Successfully!");
+          })
+          .catch((error) => {
+            setLoading(false);
+            alert(error);
+          });
+      }}
       user={{ _id: 1 }}
     />
+    )}
+    </AuthContext.Consumer>
   );
 }
+export default MessageScreen; 
